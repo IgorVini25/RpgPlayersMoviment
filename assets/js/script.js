67d18f79ff736130cb253276f7309e494eafdc76
Igor Vinicius <igvinps2006@gmail.com>
2022-10-05 21:38:54 -0300

    :bug: git bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,57 @@
 let selectedToken = null
 $('.move-wrapper').each((index, val) => {
-  $(val).draggable();
+  $(val).draggable()
   $(val).click(() => {
-    $(".move-wrapper").removeClass('selected')
+    $('.move-wrapper').removeClass('selected')
     if (index !== selectedToken) {
-      selectedToken = index;
-      $(val).addClass('selected');
+      selectedToken = index
+      $(val).addClass('selected')
     } else {
       selectedToken = null
-      $(val).removeClass('selected')
     }
   })
 })
 
+$('.trash').droppable()
+$('.trash').on('drop', function (event, ui) {
+  $(ui.draggable).remove()
+})
+
 // Transform Touch Events into mouse events
 function touchHandler(event) {
-  var touch = event.changedTouches[0];
+  var touch = event.changedTouches[0]
 
-  var simulatedEvent = document.createEvent("MouseEvent");
-      simulatedEvent.initMouseEvent({
-      touchstart: "mousedown",
-      touchmove: "mousemove",
-      touchend: "mouseup"
-  }[event.type], true, true, window, 1,
-      touch.screenX, touch.screenY,
-      touch.clientX, touch.clientY, false,
-      false, false, false, 0, null);
+  var simulatedEvent = document.createEvent('MouseEvent')
+  simulatedEvent.initMouseEvent(
+    {
+      touchstart: 'mousedown',
+      touchmove: 'mousemove',
+      touchend: 'mouseup'
+    }[event.type],
+    true,
+    true,
+    window,
+    1,
+    touch.screenX,
+    touch.screenY,
+    touch.clientX,
+    touch.clientY,
+    false,
+    false,
+    false,
+    false,
+    0,
+    null
+  )
 
-  touch.target.dispatchEvent(simulatedEvent);
-  event.preventDefault();
+  touch.target.dispatchEvent(simulatedEvent)
+  event.preventDefault()
 }
 
 function init() {
-  document.addEventListener("touchstart", touchHandler, true);
-  document.addEventListener("touchmove", touchHandler, true);
-  document.addEventListener("touchend", touchHandler, true);
-  document.addEventListener("touchcancel", touchHandler, true);
+  document.addEventListener('touchstart', touchHandler, true)
+  document.addEventListener('touchmove', touchHandler, true)
+  document.addEventListener('touchend', touchHandler, true)
+  document.addEventListener('touchcancel', touchHandler, true)
 }
 init()
